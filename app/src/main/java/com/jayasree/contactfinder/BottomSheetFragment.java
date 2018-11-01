74532581f8d630704aece098e1449ab17b6eaf1c
ptsdeen2007 <ptsdeen2007@gmail.com>
2018-11-01 20:29:03 +0530

    added bottomSheet added action to call and email

--- app/src/main/java/com/jayasree/contactfinder/BottomSheetFragment.java
+++ app/src/main/java/com/jayasree/contactfinder/BottomSheetFragment.java
@@ -1,5 +1,7 @@
 package com.jayasree.contactfinder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,9 +50,9 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         String taluk = getArguments().getString(COLUMN_TALUK);
         String village = getArguments().getString(COLUMN_VILLAGE);
         String address = getArguments().getString(COLUMN_ADDRESS);
-        String land_no = getArguments().getString(COLUMN_LAND_NO);
-        String moblie_no = getArguments().getString(COLUMN_MOBLIE_NO);
-        String email = getArguments().getString(COLUMN_EMAIL);
+        final String land_no = getArguments().getString(COLUMN_LAND_NO);
+        final String moblie_no = getArguments().getString(COLUMN_MOBLIE_NO);
+        final String email = getArguments().getString(COLUMN_EMAIL);
 
         TextView tv_taluk = getView().findViewById(R.id.tv_taluk);
         TextView tv_village = getView().findViewById(R.id.tv_village);
@@ -64,6 +67,37 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         tv_land_no.setText(land_no);
         tv_moblie_no.setText(moblie_no);
         tv_email.setText(email);
+
+        LinearLayout ll_land_number=getView().findViewById(R.id.ll_land_number);
+        LinearLayout ll_mob_number=getView().findViewById(R.id.ll_mob_number);
+        LinearLayout ll_email_id=getView().findViewById(R.id.ll_email_id);
+
+        ll_land_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:" + land_no));
+                startActivity(callIntent);
+            }
+        });
+        ll_mob_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:" + moblie_no));
+                startActivity(callIntent);
+
+            }
+        });
+        ll_email_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("mailto:"+email));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "email_subject");
+                intent.putExtra(Intent.EXTRA_TEXT, "email_body");
+                startActivity(intent);
+            }
+        });
 
     }
 }
